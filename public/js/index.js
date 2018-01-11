2f0fe2c610082fbd85a962b9d5e78c35aa147ef1
Adam Pugh <adampugh@zoho.com>
2018-01-11 01:26:22 +0000

    Added loader and updated get request

--- public/js/index.js
+++ public/js/index.js
@@ -31,7 +31,32 @@ $(".contact-btn").on("click", (e) => {
 
 
 
-
+// creates listing panels
+function addDiv(property) {
+    $(".properties").append(
+        `<div class="panel">
+        <img src="${property.image}" alt="">
+        <div class="panel__text">
+            <h2>${property.name}</h2>
+            <h3>${property.location}</h3>
+            <div class="panel__text__details">
+                <div>
+                    <h3>${property.beds}<i class="fa fa-bed" aria-hidden="true"></i></h3>
+                    <h5>Beds</h5>
+                </div>
+                <div>
+                    <h3>${property.baths}<i class="fa fa-bath" aria-hidden="true"></i></h3>
+                    <h5>Baths</h5>
+                </div>
+                <div></div>
+                <div class="price">
+                    <h3>${property.price}</h3>
+                </div>
+            </div>
+        </div>     
+    </div>`
+    );
+};
 
 
 
@@ -40,41 +65,13 @@ $(".contact-btn").on("click", (e) => {
 let url = "https://parker-api.herokuapp.com/properties";
 
 $.get(url, function(data) {
-    const properties = JSON.parse(data);
+    $(".loader__container").hide();
+
+    JSON.parse(data).map(property => {
+        addDiv(property);
+    });
 
     $(".grey-banner__text").append(`<h1>${properties.length} Luxury Homes for Sale in London`);
-
-    // creates listing panels
-    function addDiv(property) {
-        $(".properties").append(
-            `<div class="panel">
-            <img src="${property.image}" alt="">
-            <div class="panel__text">
-                <h2>${property.name}</h2>
-                <h3>${property.location}</h3>
-                <div class="panel__text__details">
-                    <div>
-                        <h3>${property.beds}<i class="fa fa-bed" aria-hidden="true"></i></h3>
-                        <h5>Beds</h5>
-                    </div>
-                    <div>
-                        <h3>${property.baths}<i class="fa fa-bath" aria-hidden="true"></i></h3>
-                        <h5>Baths</h5>
-                    </div>
-                    <div></div>
-                    <div class="price">
-                        <h3>${property.price}</h3>
-                    </div>
-                </div>
-            </div>     
-        </div>`
-        );
-    }
-
-    for (let i = 0; i < properties.length; i++) {
-        addDiv(properties[i]);
-    }
-
 });
 
 
